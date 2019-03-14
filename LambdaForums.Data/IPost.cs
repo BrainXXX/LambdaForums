c95962e2d4129be6a5bdd3a21921bbc991848764
BrainX <xxxbrainxxx@gmail.com>
2019-03-15 00:25:33 +1000

    Vid5

---
+++ LambdaForums.Data/IPost.cs
@@ -0,0 +1,18 @@
+﻿using LambdaForums.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Data
+{
+    public interface IPost
+    {
+        IPost GetById(int id);
+        IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetFilteredPosts(string searchQuery);
+        IEnumerable<Post> GetPostsByForum(int id);
+
+        Task Add(IPost post);
+        Task Delete(int id);
+        Task EditPostContent(int id, string newContent);      
+    }
+}
